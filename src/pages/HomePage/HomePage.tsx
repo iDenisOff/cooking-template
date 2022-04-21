f4cc9a327b4fafaaf8cffee1ea75a963e26235dd
Danila Denisov <andreyfrolovdda@gmail.com>
2022-04-21 17:30:15 +0300

    Created Categories

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./HomePage.style.css";
 import { Header } from "./childs/Header";
+import {Categories} from "./childs/Categories";
 
 export const HomePage: React.FC = () => {
     return (
         <div className={styles.container}>
             <Header />
+            <Categories />
         </div>
     );
 };
