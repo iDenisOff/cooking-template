4f1c85613aa4a603082003232fb735e19d4f5d41
Danila Denisov <andreyfrolovdda@gmail.com>
2022-05-25 22:49:57 +0300

    Finished Blog Post Page

--- src/pages/BlogPostPage/childs/PostContent/PostContent.tsx
+++ src/pages/BlogPostPage/childs/PostContent/PostContent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./PostContent.style.css";
+import cookAtHome from "./content/cookAtHome.png";
 
 export const PostContent: React.FC = () => {
     return (
@@ -23,6 +24,7 @@ export const PostContent: React.FC = () => {
             <p className={styles.title}>
                 {"Do you cook at home on your days off?"}
             </p>
+            <img src={cookAtHome} alt={"cookAtHome"} style={{ paddingBottom: "32px"}}/>
             <p className={styles.text}>
                 {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio. "}
                 {"Nulla at congue diam, at dignissim turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. "}
@@ -36,10 +38,13 @@ export const PostContent: React.FC = () => {
                 {"Nulla at congue diam, at dignissim turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. "}
                 {"Fusce id mollis ex. Praesent feugiat elementum ex ut suscipit."}
             </p>
+            <div className={styles.quoteContainer}>
+                <p className={styles.quoteText}>{"“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio.”"}</p>
+            </div>
             <p className={styles.title}>
                 {"What is the biggest misconception that people have about being a professional chef?"}
             </p>
-            <p className={styles.text}>
+            <p className={styles.text} style={{ marginBottom: "0px" }}>
                 {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices odio. "}
                 {"Nulla at congue diam, at dignissim turpis. Ut vehicula sed velit a faucibus. In feugiat vestibulum velit vel pulvinar. "}
                 {"Fusce id mollis ex. Praesent feugiat elementum ex ut suscipit."}
